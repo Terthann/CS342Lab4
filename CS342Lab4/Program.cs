bf62e6c403f70c1f8d5446ddcc3ec16bab5ea2b2
Terthann <60939529+Terthann@users.noreply.github.com>
2021-04-15 14:17:55 -0700

    Created Functions

--- CS342Lab4/Program.cs
+++ CS342Lab4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CS342Lab4
 {
@@ -10,6 +11,21 @@ namespace CS342Lab4
     {
         static void Main(string[] args)
         {
+        }
+
+        static IEnumerable<string> EnumerateFilesRecursively(string path)
+        {
+            yield return "";
+        }
+
+        static string FormatByteSize(long byteSize)
+        {
+            return "";
+        }
+
+        static XDocument CreateReport(IEnumerable<string> files)
+        {
+            return new XDocument();
         }
     }
 }
