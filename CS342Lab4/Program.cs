68977a00258f91f673a7070edb4d9099db258765
Terthann <60939529+Terthann@users.noreply.github.com>
2021-04-15 15:07:37 -0700

    Partially Working Functions

--- CS342Lab4/Program.cs
+++ CS342Lab4/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using System.IO;
 
 namespace CS342Lab4
 {
@@ -11,11 +12,15 @@ namespace CS342Lab4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(CreateReport(EnumerateFilesRecursively("D:\\CS342Lab4")));
+            Console.ReadLine();
         }
 
+        // Enumerate all files in a given folder recursively. (Including entire sub-folder hierarchy)
         static IEnumerable<string> EnumerateFilesRecursively(string path)
         {
-            yield return "";
+            foreach (string s in Directory.EnumerateFiles(path))
+                yield return s;
         }
 
         static string FormatByteSize(long byteSize)
@@ -25,7 +30,9 @@ namespace CS342Lab4
 
         static XDocument CreateReport(IEnumerable<string> files)
         {
-            return new XDocument();
+            return new XDocument(new XElement("table", new XElement("tr",
+                from f in files.AsEnumerable() select
+                    new XElement("th", new FileInfo(f).Name))));
         }
     }
 }
