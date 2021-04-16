6a18e839e74bb5559d7b1059756b39ec3a19d224
Terthann <60939529+Terthann@users.noreply.github.com>
2021-04-15 19:47:38 -0700

    Mostly Finished CreateReport and Enumerate Functions

--- CS342Lab4/Program.cs
+++ CS342Lab4/Program.cs
@@ -13,12 +13,21 @@ namespace CS342Lab4
         static void Main(string[] args)
         {
             Console.WriteLine(CreateReport(EnumerateFilesRecursively("D:\\CS342Lab4")));
+            CreateReport(EnumerateFilesRecursively("D:\\CS342Lab4")).Save("D:\\CS342Lab4\\index.html");
             Console.ReadLine();
         }
 
         // Enumerate all files in a given folder recursively. (Including entire sub-folder hierarchy)
         static IEnumerable<string> EnumerateFilesRecursively(string path)
         {
+            if (Directory.EnumerateDirectories(path).Count() > 0)
+            {
+                foreach (string d in Directory.EnumerateDirectories(path))
+                    foreach (string s in EnumerateFilesRecursively(d))
+                        yield return s;
+            }
+
+            //foreach (string d in Directory.EnumerateDirectories("D:\\CS342Lab4"))
             foreach (string s in Directory.EnumerateFiles(path))
                 yield return s;
         }
@@ -28,11 +37,22 @@ namespace CS342Lab4
             return "";
         }
 
+        // Create a HTML document containing a table with 3 columns:
+        // "Type"   - File name extension (lower case).
+        // "Count"  - Number of files with this type.
+        // "Size"   - Total size of all files with this type.
         static XDocument CreateReport(IEnumerable<string> files)
         {
-            return new XDocument(new XElement("table", new XElement("tr",
-                from f in files.AsEnumerable() select
-                    new XElement("th", new FileInfo(f).Name))));
+            return new XDocument(new XElement("table",
+                new XElement("tr",
+                    new XElement("th", "Type"),
+                    new XElement("th", "Count"),
+                    new XElement("th", "Size")),
+                from f in files.AsEnumerable().GroupBy(file => Path.GetExtension(file)).OrderBy(a => a.Sum(file => new FileInfo(file).Length)).Reverse() select
+                    new XElement("tr",
+                        new XElement("td", f.Count()),
+                        new XElement("td", f.Key),
+                        new XElement("td", f.Sum(file => new FileInfo(file).Length)))));
         }
     }
 }
