73f08a6a83b4a51a7502ec5c21e0d75590f5ec42
Terthann <60939529+Terthann@users.noreply.github.com>
2021-04-15 19:54:00 -0700

    Added Comments

--- CS342Lab4/Program.cs
+++ CS342Lab4/Program.cs
@@ -12,22 +12,27 @@ namespace CS342Lab4
     {
         static void Main(string[] args)
         {
+            // Writes the report to the console.
             Console.WriteLine(CreateReport(EnumerateFilesRecursively("D:\\CS342Lab4")));
+            // Saves the report to the specified path.
             CreateReport(EnumerateFilesRecursively("D:\\CS342Lab4")).Save("D:\\CS342Lab4\\index.html");
+            // Pauses the console.
             Console.ReadLine();
         }
 
         // Enumerate all files in a given folder recursively. (Including entire sub-folder hierarchy)
         static IEnumerable<string> EnumerateFilesRecursively(string path)
         {
+            // Check if there are any more subdirectories.
             if (Directory.EnumerateDirectories(path).Count() > 0)
             {
+                // If there are, recursively call.
                 foreach (string d in Directory.EnumerateDirectories(path))
                     foreach (string s in EnumerateFilesRecursively(d))
                         yield return s;
             }
 
-            //foreach (string d in Directory.EnumerateDirectories("D:\\CS342Lab4"))
+            // For each file in this directory.
             foreach (string s in Directory.EnumerateFiles(path))
                 yield return s;
         }
@@ -43,11 +48,14 @@ namespace CS342Lab4
         // "Size"   - Total size of all files with this type.
         static XDocument CreateReport(IEnumerable<string> files)
         {
+            // Creates table.
             return new XDocument(new XElement("table",
+                // Create row of headers.
                 new XElement("tr",
                     new XElement("th", "Type"),
                     new XElement("th", "Count"),
                     new XElement("th", "Size")),
+                // Create a row per file extension group. (Descending order based on "Size")
                 from f in files.AsEnumerable().GroupBy(file => Path.GetExtension(file)).OrderBy(a => a.Sum(file => new FileInfo(file).Length)).Reverse() select
                     new XElement("tr",
                         new XElement("td", f.Count()),
